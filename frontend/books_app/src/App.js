64d370cf9a90041342576594b56f6f409b68c8c6
amnw12 <48080226+amnw12@users.noreply.github.com>
2022-04-01 23:37:19 +1300

    finished frontend and backend

---
+++ frontend/books_app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ShowBookList />} />
+        <Route path="/create-book" element={<CreateBook />} />
+        <Route path="/edit-book/:id" element={<UpdateBookInfo/>} />
+        <Route path="/show-book/:id" element={ <ShowBookDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
